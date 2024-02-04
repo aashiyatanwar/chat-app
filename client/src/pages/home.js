0cec66e3e1a950888933c2eef39121ae9c73e6e5
aashiyatanwar <tanwaraashiya12@gmail.com>
2024-02-04 13:14:44 +0530

    First Commit

---
+++ client/src/pages/home.js
@@ -0,0 +1,11 @@
+import Message from "./message/index";
+
+const Home = () => {
+  return (
+    <div className="home row mx-0">
+      <Message></Message>
+    </div>
+  );
+};
+
+export default Home;
